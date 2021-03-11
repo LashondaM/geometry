ecac8011c41ce35744a98f7e5f2214358598fee5
Lashonda <lashondamiller007@gmail.com>
2021-03-11 17:40:55 -0500

    created 3D ring

---
+++ js/geo.js
@@ -0,0 +1,90 @@
+let scene, camera, renderer, torus;
+
+function init(){
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer({antialias : true});
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+    // const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+
+    const texture = new THREE.TextureLoader().load('textures/FloorsCheckerboard_S_Normal.jpg')
+    const material = new THREE.MeshBasicMaterial( { map: texture } );
+    torus = new THREE.Mesh( geometry, material );
+    scene.add( torus );
+
+    camera.position.z = 55;
+
+}
+
+const animate = function () {
+	requestAnimationFrame( animate );
+
+	torus.rotation.x += 0.05;
+	torus.rotation.y += 0.05;
+
+	renderer.render( scene, camera );
+	};
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    init();
+	animate();
+
+    
+// let scene, camera, renderer, torus;
+
+// function init(){
+//     scene = new THREE.Scene();
+
+//     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//     renderer = new THREE.WebGLRenderer({antialias : true});
+
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+
+//     document.body.appendChild( renderer.domElement );
+
+//     const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+//     // const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+
+//     const texture = new THREE.TextureLoader().load('textures/FloorsCheckerboard_S_Normal.jpg')
+//     const material = new THREE.MeshBasicMaterial( { map: texture } );
+//     torus = new THREE.Mesh( geometry, material );
+//     scene.add( torus );
+
+//     camera.position.z = 55;
+
+// }
+
+// const animate = function () {
+// 	requestAnimationFrame( animate );
+
+// 	torus.rotation.x += 0.05;
+// 	torus.rotation.y += 0.05;
+
+// 	renderer.render( scene, camera );
+// 	};
+
+//     function onWindowResize() {
+//         camera.aspect = window.innerWidth / window.innerHeight;
+//         camera.updateProjectionMatrix();
+//         renderer.setSize( window.innerWidth, window.innerHeight );
+//     }
+
+//     window.addEventListener('resize', onWindowResize, false);
+
+//     init();
+// 	animate();
